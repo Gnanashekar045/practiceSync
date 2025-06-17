1087764708f8dbd6265174db5208c7da331432b5
Gnanashekar K <gnanashekar045@gmail.com>
2025-06-17 11:50:42 +0530

    ArrayOfObjectOr is comming

---
+++ interviewcodes/src/components/ArrayOfObjectOr.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const ArrayOfObjectOr = () => {
+    const optionsListed = [
+    { id: 1, label: "JavaScript" },
+    { id: 2, label: "Python" },
+    { id: 3, label: "Java" }
+  ];
+
+  const [selected, setSelected] = useState("");
+
+  return (
+    <>
+    <h3>Select Language</h3>
+    <select value={selected} onChange={(e)=>{setSelected(e.target.value)}}>
+        <option value="">Select</option>
+        {optionsListed.map((opt)=>(
+            <option value={opt.label} key={opt.id}>{opt.label}</option>
+        ))}
+    </select>
+    <p>You have selected: {selected}</p>
+    </>
+  )
+}
+
+export default ArrayOfObjectOr
